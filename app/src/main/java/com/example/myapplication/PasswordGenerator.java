c62693987b7a5232e8e3480d7c097ba0659a2420
nguyenthanhson162001 <nguyenthanhson162001@gmail.com>
2022-02-24 13:57:10 +0700

    'finish-view-3'

---
+++ app/src/main/java/com/example/myapplication/PasswordGenerator.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PasswordGenerator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_generator);
+    }
+}
